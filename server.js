f594dd0ee2614c0ba0d1807869eea7a46504bc23
LK <lorikxi@icloud.com>
2020-07-22 18:58:41 +0200

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [{
+        name: 'Mark'
+    },
+    {
+        name: 'Jill'
+    }
+]
+app.get('/users', (request, response) => {
+    response.json({
+        success: true,
+        message: 'Succesfully got users. Nice!',
+        users: mockUserData
+    })
+});
+
 app.listen(8000, () => {
     console.log(`Server started on port 8K`);
 });
